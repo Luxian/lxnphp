bb5cac282cbab742792e86422436115ae9b72b2b
Luxian <luxian.m@gmail.com>
2014-08-28 11:35:16 +0200

    Improve function code + change filename extension

--- function/array_insert_at_position.php
+++ function/array_insert_at_position.php
@@ -28,22 +28,32 @@
  *   The resulted array with $values inserted a given position
  */
 function array_insert_at_position($array, $values, $pivot, $position = 'after'){
-  $offset = 0;
-  foreach($array as $key => $value){
-    ++$offset;
-    if ($key == $pivot){
-      break;
-    }
+/**
+ * Insert values in a associative array at a given position
+ *
+ * @param array $array
+ *   The array in which you want to insert
+ * @param array $values
+ *   The key => values you want to insert
+ * @param string $pivot
+ *   The key position to use as insert position.
+ * @param string $position
+ *   Where to insert the values relative to given $position.
+ *   Allowed values: 'after' - default or 'before'
+ *
+ * @return array
+ *   The resulted array with $values inserted a given position
+ */
+function array_insert_at_position($array, $values, $pivot, $position = 'after'){
+  $array_keys = array_keys($array);
+  $offset = array_search($pivot, $array_keys);
+  if ($position == 'after') {
+    $offset++;
   }
 
-  if ($position == 'before'){
-    --$offset;
-  }
-
-  return
-    array_slice($array, 0, $offset, TRUE)
+  return array_slice($array, 0, $offset, TRUE)
     + $values
-    + array_slice($array, $offset, NULL, TRUE)
-  ;
+    + array_slice($array, $offset, NULL, TRUE);
+}
 }
 
