66f896630d82a9c9581fdee9e40e91d253ff10e3
Luxian <luxian.m@gmail.com>
2014-08-28 11:35:45 +0200

    Update array_insert_at_position.php

--- function/array_insert_at_position.php
+++ function/array_insert_at_position.php
@@ -10,24 +10,6 @@
  * 	http://stackoverflow.com/questions/1783089/array-splice-for-associative-arrays
  */
 
-
-/**
- * Insert values in a associative array at a given position
- *
- * @param array $array
- *   The array in which you want to insert
- * @param array $values
- *   The key => values you want to insert
- * @param string $pivot
- *   The key position to use as insert position.
- * @param string $position
- *   Where to insert the values relative to given $position.
- *   Allowed values: 'after' - default or 'before'
- *
- * @return array
- *   The resulted array with $values inserted a given position
- */
-function array_insert_at_position($array, $values, $pivot, $position = 'after'){
 /**
  * Insert values in a associative array at a given position
  *
@@ -54,6 +36,5 @@ function array_insert_at_position($array, $values, $pivot, $position = 'after'){
   return array_slice($array, 0, $offset, TRUE)
     + $values
     + array_slice($array, $offset, NULL, TRUE);
-}
 }
 
